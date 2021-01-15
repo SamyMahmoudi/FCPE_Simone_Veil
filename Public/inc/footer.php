212dbe15a208a975cc41a08c690acaa9c1965196
Samy Mahmoudi <samy.mahmoudi@edu.devinci.fr>
2021-01-15 10:49:08 +0100

    donation system

--- Public/inc/footer.php
+++ Public/inc/footer.php
@@ -25,6 +25,9 @@
         </nav>
     </section>
     <p>FCPE Simone Veil - <a href="?page=mentions" title="Mentions Légales">Mentions Légales</a></p>
+    <div class="BtnDon">
+        <iframe  id="haWidget" allowtransparency="true" src="https://www.helloasso.com/associations/fcpe-simone-veil/formulaires/1/widget-bouton" style="width:100%;height:70px;border:none;padding-top:5%;"></iframe>
+    </div>
 </footer>
 
 <script src="js/ResponsiveMenu.js"></script>
